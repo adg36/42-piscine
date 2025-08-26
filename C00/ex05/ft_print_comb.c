98fc94c5ab937dafbf6bdc822e40d0019d2cc88d
Renata <re.moreirazevedo@gmail.com>
2025-08-26 14:45:08 +0100

    C00 ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,17 +1,40 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_print_comb(void)
 {
-	char	a;
-	char	b;
-	char	c;
+	int	a;
+	int	b;
+	int	c;
 
 	a = 0;
-	b = 1;
-	c = 2;
-
-	while (a <= 7 && b <= 8 && c <= 9) 
+	while (a <= 7) 
 	{
-		
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				ft_putchar(a + '0');
+				ft_putchar(b + '0');
+				ft_putchar(c + '0');
+
+				if (a != 7)
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				else
+					ft_putchar('\n');
+				c++;
+			}
+			b++;
+		}
+		a++;
 	}
 }
