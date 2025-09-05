d39c650b0947e25c07b126fa22921712a7e87168
Renata <re.moreirazevedo@gmail.com>
2025-09-05 21:21:32 +0100

    C03

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -5,26 +5,18 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
-	char	*found;
 
+	if (*to_find = '\0')
+		return (str);
 	i = 0;
-	j = 0;
 	while (str[i])
 	{
-		found = &str[i];
-		while (str[i] == to_find[j] && to_find[j] != '\0')
-		{
-			i++;
+		j = 0;
+		while (str[i + j] && to_find[j] && str[i + j] == to_find[j])
 			j++;
-		}
 		if (to_find[j] == '\0')
-			return (found);
-		else if (str[i] != to_find[j] && to_find[j] !='\0') 
-		{
-			j = 0;
-			i++;
-		}
-
+			return (&str[i]);
+		i++;
 	}
 	return (NULL);
 }
