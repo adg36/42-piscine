08142faa4d181e8d1b4a478aac9f2ea0c71636cf
Renata <re.moreirazevedo@gmail.com>
2025-09-08 18:10:59 +0100

    C04 ex00, ex01, ex02

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+	char	c;
+
+	n = nb;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
