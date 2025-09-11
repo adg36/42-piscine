08da75e83331f44759b5e6b68d61de6053fcd5c2
Renata <re.moreirazevedo@gmail.com>
2025-09-11 17:33:15 +0100

    C04 ex04

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -1,11 +1,27 @@
 #include <unistd.h>
 
 int	ft_strlen(char *str);
-int	is_valid_base(char *str);
+int	is_valid_base(char *base);
 
 void	ft_putnbr_base(int nbr, char *base)
 {
+	long	n;
+	char	c;
+	int	base_len;
 
+	base_len = ft_strlen(base);
+	if (!is_valid_base(base))
+		return;
+	n = nbr;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= base_len)
+		ft_putnbr_base(n / base_len, base);
+	c = base[n % base_len];
+	write(1, &c, 1);
 }
 
 int	ft_strlen(char *str)
@@ -18,7 +34,28 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int	is_valid_base(char *str)
+int	is_valid_base(char *base)
 {
+	int	i;
+	int	j;
+	int	base_len;
 
+	base_len = ft_strlen(base);
+	if (base_len == 0 || base_len == 1)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		j = i + 1;
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
