3e558b32d5809b72fb705063f901fc06dfe93b01
Renata <re.moreirazevedo@gmail.com>
2025-09-18 15:15:09 +0100

    In progress

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+int	ft_atoi_base(char *str, char *base);
+int	count_digits(int n, int base_len);
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	len_base_from;
+	int	len_base_to;
+	int	converted_nbr;
+	char	*result;
+
+	len_base_from = ft_strlen(base_from);
+	len_base_to = ft_strlen(base_to);
+	if (!is_valid_base(base_from, len_base_from) || !is_valid_base(base_to, len_base_to))
+		return (NULL);
+	converted_nbr = ft_atoi_base(nbr, base_from);
+	if (converted_nbr < 0)
+		result = malloc(count_digits(converted_nbr, len_base_to) + 2);
+	else
+		result = malloc(count_digits(converted_nbr, len_base_to) + 1);
+	if (!result)
+		return (NULL);
+
+}
+
+int	count_digits(int n, int base_len)
+{
+	int	count;
+
+	count = 0;
+	while (n >= base_len)
+	{
+		n /= base_len;
+		count++;
+	}
+	return (count);
+}
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,102 @@
+int	ft_strlen(char *str);
+int	is_valid_base(char *base, int base_len);
+int	is_valid_char(char c, char *base);
+int	get_index(char c, char *base);
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	n;
+	int	minus;
+	int	base_len;
+	
+	base_len = ft_strlen(base);
+	if (!is_valid_base(base, base_len))
+		return (0);
+	n = 0;
+	minus = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			minus++;
+		str++;
+	}
+	if (minus % 2 != 0)
+		minus = 1;
+	else
+		minus = 0;
+	while (is_valid_char(*str, base))
+	{
+		n = n * base_len + get_index(*str, base);
+		str++;
+	}
+	if (minus)
+		return (-n);
+	return (n);
+}
+
+int	is_valid_base(char *base, int base_len)
+{
+	int	i;
+	int	j;
+
+	if (base_len == 0 || base_len == 1)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' ')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	is_valid_char(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	get_index(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (*base)
+	{
+		if (c == *base)
+		{
+			break;
+		}
+		base++;
+		i++;
+	}
+	return (i);
+}
